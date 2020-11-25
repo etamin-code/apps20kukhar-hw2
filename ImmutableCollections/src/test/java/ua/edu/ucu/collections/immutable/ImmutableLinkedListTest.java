550fe6e7a23517606e5800989687524d82f83ebe
etamin-code <physicist.o.kuhar@gmail.com>
2020-11-26 00:15:39 +0200

    initial commit

--- ImmutableCollections/src/test/java/ua/edu/ucu/collections/immutable/ImmutableLinkedListTest.java
+++ ImmutableCollections/src/test/java/ua/edu/ucu/collections/immutable/ImmutableLinkedListTest.java
@@ -5,8 +5,154 @@ import static org.junit.Assert.*;
 
 public class ImmutableLinkedListTest {
     
+//    @Test
+//    public void testAdd1() throws CloneNotSupportedException {
+//        Node node1 = new Node(), node2 = new Node(), node3 = new Node(), node4 = new Node();
+//        node1.setObject(1);
+//        node2.setObject(2);
+//        node3.setObject(3);
+//        node4.setObject(4);
+//
+//        node1.setNext(node2);
+//        node2.setNext(node3);
+//        node3.setNext(node4);
+//
+//        node2.setPrevious(node1);
+//        node3.setPrevious(node2);
+//        node4.setPrevious(node3);
+//
+//        ImmutableLinkedList immutableLinkedList = new ImmutableLinkedList(node1, node4, 4);
+//        ImmutableLinkedList immutableLinkedList1 = immutableLinkedList.copy();
+//        System.out.println(immutableLinkedList1.getFirst());
+//        System.out.println(immutableLinkedList1.get(0));
+//        System.out.println(immutableLinkedList1.get(1));
+//        System.out.println(immutableLinkedList1.get(2));
+//        System.out.println(immutableLinkedList1.get(3));
+//        System.out.println(immutableLinkedList1.getLast());
+//
+//
+//
+//    }
+
     @Test
-    public void testSomeMethod() {
+    public void testAdd() {
+        ImmutableList immutableLinkedList1 = new ImmutableLinkedList();
+        ImmutableList immutableLinkedList2 = immutableLinkedList1.add(5);
+        assertSame(5, immutableLinkedList2.get(0));
+        immutableLinkedList1 = immutableLinkedList2.add(6);
+        assertSame(5, immutableLinkedList1.get(0));
+        assertSame(6, immutableLinkedList1.get(1));
+        immutableLinkedList2 = immutableLinkedList1.add(7);
+        assertSame(5, immutableLinkedList2.get(0));
+        assertSame(6, immutableLinkedList2.get(1));
+        assertSame(7, immutableLinkedList2.get(2));
+        immutableLinkedList1 = immutableLinkedList2.add(1, 10);
+        assertSame(10, immutableLinkedList1.get(1));
     }
-    
+
+    @Test
+    public void testAddAll() {
+        ImmutableList immutableLinkedList1 = new ImmutableLinkedList();
+
+        Object[] array_to_add = {1, 2, 3};
+        ImmutableList immutableLinkedList2 = immutableLinkedList1.addAll(array_to_add);
+
+        assertSame(1, immutableLinkedList2.get(0));
+        assertSame(2, immutableLinkedList2.get(1));
+        assertSame(3, immutableLinkedList2.get(2));
+
+        array_to_add = new Object[]{5, 6, 7};
+        immutableLinkedList1 = immutableLinkedList2.addAll(1, array_to_add);
+        assertSame(1, immutableLinkedList1.get(0));
+        assertSame(5, immutableLinkedList1.get(1));
+        assertSame(6, immutableLinkedList1.get(2));
+        assertSame(7, immutableLinkedList1.get(3));
+
+    }
+
+    @Test
+    public void testRemove() {
+        ImmutableList immutableLinkedList1 = new ImmutableLinkedList();
+
+        Object[] array_to_add = {1, 2, 3};
+        ImmutableList immutableLinkedList2 = immutableLinkedList1.addAll(array_to_add);
+        immutableLinkedList1 = immutableLinkedList2.remove(1);
+
+        assertSame(1, immutableLinkedList1.get(0));
+        assertSame(3, immutableLinkedList1.get(1));
+        immutableLinkedList2 = immutableLinkedList1.remove(0);
+
+        assertSame(3, immutableLinkedList2.get(0));
+        immutableLinkedList1 = immutableLinkedList2.remove(0);
+
+        assertTrue(immutableLinkedList1.isEmpty());
+
+    }
+
+    @Test
+    public void testSet() {
+        ImmutableList immutableLinkedList1 = new ImmutableLinkedList();
+
+        Object[] array_to_add = {1, 2, 3};
+        ImmutableList immutableLinkedList2 = immutableLinkedList1.addAll(array_to_add);
+        immutableLinkedList1 = immutableLinkedList2.set(0, 9);
+
+        assertSame(9, immutableLinkedList1.get(0));
+        assertSame(2, immutableLinkedList1.get(1));
+        assertSame(3, immutableLinkedList1.get(2));
+
+        immutableLinkedList2 = immutableLinkedList1.set(1, 9);
+
+        assertSame(9, immutableLinkedList2.get(0));
+        assertSame(9, immutableLinkedList2.get(1));
+        assertSame(3, immutableLinkedList2.get(2));
+
+        immutableLinkedList1 = immutableLinkedList2.set(2, 9);
+        assertSame(9, immutableLinkedList1.get(0));
+        assertSame(9, immutableLinkedList1.get(1));
+        assertSame(9, immutableLinkedList1.get(2));
+
+    }
+
+    @Test
+    public void testIndex() {
+        ImmutableList immutableLinkedList1 = new ImmutableLinkedList();
+
+        Object[] array_to_add = {1, 2, 3, 1};
+        ImmutableList immutableLinkedList2 = immutableLinkedList1.addAll(array_to_add);
+
+        assertSame(0, immutableLinkedList2.indexOf(1));
+        assertSame(1, immutableLinkedList2.indexOf(2));
+        assertSame(2, immutableLinkedList2.indexOf(3));
+        assertSame(-1, immutableLinkedList2.indexOf(4));
+        immutableLinkedList1 = immutableLinkedList2.remove(0);
+        assertSame(2, immutableLinkedList1.indexOf(1));
+
+    }
+
+    @Test
+    public void testClear() {
+        ImmutableList immutableLinkedList1 = new ImmutableLinkedList();
+
+        Object[] array_to_add = {1, 2, 3, 1};
+        ImmutableList immutableLinkedList2 = immutableLinkedList1.addAll(array_to_add);
+
+        immutableLinkedList1 = immutableLinkedList2.clear();
+        assertSame(0, immutableLinkedList1.size());
+
+    }
+
+    @Test
+    public void testToArray() {
+        ImmutableList immutableLinkedList1 = new ImmutableLinkedList();
+
+        Object[] array_to_add = {1, 2, 3, 1};
+        ImmutableList immutableLinkedList2 = immutableLinkedList1.addAll(array_to_add);
+
+        Object arr[] = immutableLinkedList2.toArray();
+
+        assertArrayEquals(arr, array_to_add);
+
+    }
+
 }
